3bc343f5c7ce887b97e1620878b6730c9902daa1
Jesper <lemuringen91113@gmail.com>
2021-02-05 19:45:46 +0100

    Some small changes so that it can modify html to show a cinema from a Cinema object.

--- SeatingsController.js
+++ SeatingsController.js
@@ -1,6 +1,6 @@
 import Cinema from '/Cinema.js';
 //create a cinema with 4x4 seats
-let cinema = new Cinema([5, 4, 4, 4]);
+let cinema = new Cinema([5, 4, 7, 4]);
 populateCinemaGUI(cinema);
 
 
@@ -11,7 +11,7 @@ function populateCinemaGUI(cinema) {
   //clear .seat-selectors before we update it
   $('.seat-selectors').html('');
   //How many columns does the cinema have?
-  $('.seat-selectors').css('grid-template-columns', `repeat(${cinema.maxRowWidth}, 1fr)`);
+  //$('.seat-selectors').css('grid-template-columns', `repeat(${cinema.maxRowWidth}, 1fr)`);
   console.log(cinema.maxRowWidth);
 
   for (let row = 0; row < cinema.seatings.length; row++) {
@@ -19,5 +19,6 @@ function populateCinemaGUI(cinema) {
       $('.seat-selectors').append(`<button type="button" value="${col}_${row}">${col}_${row}</button>`);
 
     }
+    $('.seat-selectors').append('<br>');
   }
 }
