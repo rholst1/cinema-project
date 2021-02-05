5adb41e728aa953b2ee54b4e1894ae93196d923d
Jesper <lemuringen91113@gmail.com>
2021-02-05 21:18:55 +0100

    Added button listener that toggles button color and corresponding value in Cinema

--- SeatingsController.js
+++ SeatingsController.js
@@ -1,11 +1,15 @@
 import Cinema from '/Cinema.js';
 //create a cinema with 4x4 seats
-let cinema = new Cinema([5, 4, 7, 4]);
+let cinema = new Cinema([5, 4, 4, 4]);
 populateCinemaGUI(cinema);
-
-
-
-
+$('.seat-selectors').on('click', 'button', function () {
+  let seat = $(this).val();
+  if (cinema.toggleSeatValue(seat)) {
+    $(`:button[value="${seat}"]`).css('background-color', 'red');
+  } else {
+    $(`:button[value="${seat}"]`).css('background-color', 'green');
+  }
+})
 
 function populateCinemaGUI(cinema) {
   //clear .seat-selectors before we update it
@@ -17,7 +21,6 @@ function populateCinemaGUI(cinema) {
   for (let row = 0; row < cinema.seatings.length; row++) {
     for (let col = 0; col < cinema.seatings[row].columns.length; col++) {
       $('.seat-selectors').append(`<button type="button" value="${col}_${row}">${col}_${row}</button>`);
-
     }
     $('.seat-selectors').append('<br>');
   }
