6b05d09aa5c583b4ba06b45e1eca567674c1aa97
Jesper <lemuringen91113@gmail.com>
2021-02-05 02:49:29 +0100

    Added a getSeatValue method that returns a boolean telling wheather a seat is taken or not. Also updates the seating variable name to seatings.

--- Cinema.js
+++ Cinema.js
@@ -1,11 +1,25 @@
 import SeatRow from '/SeatRow.js';
 export default class Cinema {
   /* 2d-array of the seatings, row dimension is contained in SeatRow objects*/
-  seating = [];
+  seatings = [];
+  /*the greatest row width is saved for easy access.*/
+  maxRowWidth = 1;
   /* seating should be an array contain number of seats for each row*/
   constructor(seatings) {
+    /*Parse list of row widths and create corresponding SeatRow objects.*/
     for (let row of seatings) {
       this.seatings = new SeatRow(row);
+      /*Save the row value to maxRowWidth if it's greater*/
+      if (row > this.maxRowWidth) {
+        this.maxRowWidth = row;
+      }
     }
+  }
+  /*expects seat as string in the form {col}_{row} ex: 2_1. Returns seat booked status as boolean. */
+  getSeatValue(seat) {
+    seatArray = seat.split('_');
+    col = parseInt(seatArray[0]);
+    row = parseInt(seatArray[1]);
+    return seatings[row].columns[col];
   }
 }
