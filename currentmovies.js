c9c66973f075b2e6ed0f7380bcd6b24cb7866b62
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-02-09 15:32:50 +0100

    Created functions for getting movie info to movielist
    Marcus and Rasmus made a code-along, implementing container for a movie with proper css,. Added Oswald font. Added html with jQuery from currentmovies.js

--- currentmovies.js
+++ currentmovies.js
@@ -16,20 +16,46 @@ let movieDivs = [
 ];
 $('section').append(movieDivs);
 
-let movieIMG = `<div class="currentMovieIMG1">
-                  <img src="img/commando1985.jpg" class="img1" alt="Här ska en bild vara">
-                </div>`;
+let movieTitles = ['COMMANDO', 'THE BATMAN', 'SÄLLSKAPSRESAN'];
+let movieImages = [
+  'img/commando2.jpg',
+  'img/batman.webp',
+  'img/sallskapsresan.jpg',
+];
+let movieGenre = ['Action', 'Action', 'Komedi'];
+let movieInfo = [
+  `En pensionerad specialagent vid namn John Matrix ledde en
+                  elitenhet och har lämnat de väpnade styrkorna för att bo i
+                  ett avskilt bergshem  med sin dotter Jenny.
+                  Men nu tvingas han gå ur pension när hans dotter
+                  kidnappas av ett band av skurkar som vill hämnas!
+                  Utan att Matrixtill dödas medlemmarna i hans tidigare
+                  enhet en efter en. Även om Matrix vän General Franklin Kirby ger Matrix beväpnade vakter, lyckas angripare kidnappa Matrix och Jenny.`,
+  `Batman info`,
+  `Välkommen till våran ö, sluta tänk på regn och snö. Sola, bada och ha  kul med Suntrip över jul! Om du vill ha nattklubbssväng, häng då med i Gabbes gäng.`,
+];
+let movieLength = ['1 tim 45 min', '1 tim 57 min', '1 tim 12 min'];
+let ageMin = ['15 år', '15 år', '6 år'];
+let i = 0;
 
-let movieTitle = `<div class="currentMovieTitleDiv">
-                    <h1 class="currentMovieTitle">COMMANDO</h1> <p class="title-p">Action | 1 tim 57 min | 15 år </p><hr>
-                      <p>En pensionerad specialagent vid namn John Matrix ledde en elitenhet
-                          och har lämnat de väpnade styrkorna för att bo i ett avskilt bergshem
-                          med sin dotter Jenny. Men nu tvingas han gå ur pension när hans dotter
-          kidnappas av ett band av skurkar som vill hämnas!
+let currentTitle = `<h1 class="currentMovieTitleH1">AKTUELLA FILMER JUST NU</h1>`;
+
+$('.movies').prepend(currentTitle);
+
+function buildMoviePage() {
+  let movieHtml;
+  while (i < movieTitles.length) {
+    movieHtml = `<div class="currentMovieIMG1">
+                  <img src="${movieImages[i]}" class="img1" alt="Här ska en bild vara">
+                </div>
+                <div class="currentMovieTitleDiv">
+                    <h2 class="currentMovieTitle">${movieTitles[i]}</h2> <p class="title-p"> ${movieGenre[i]} | ${movieLength[i]} | ${ageMin[i]} </p><hr>
+                      <p>${movieInfo[i]}
                       </p>
                   </div>`;
+    i++;
+    $(`.currentMovie${i}`).append(movieHtml);
+  }
+}
 
-let movieButtons = ``;
-
-$('.currentMovie1').append(movieIMG);
-$('.currentMovie1').append(movieTitle);
+buildMoviePage();
