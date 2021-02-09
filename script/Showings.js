53bd497f259be83837c1253421569fe1fd10bb09
Jesper <lemuringen91113@gmail.com>
2021-02-09 14:51:25 +0100

    Restructuring of the internal booked status structure. Now showings lists Seat and Seat keep its own coordinates.

--- script/Showings.js
+++ script/Showings.js
@@ -1,11 +1,28 @@
+import Seat from "/script/Seat.js";
+
 export default class Showings {
   constructor(auditorium, film, date, reservedSeats) {
-    this.auditorium = auditorium;
+    this.auditorium = auditorium; // Cinema
     this.film = film;
     this.date = date; // Date
-    this.reservedSeats = [];
+    this.seats = [];
+    for (let row of auditorium.seatsPerRow) {
+      for (let col = 0; 0 < row; col++) {
+        this.seats.push(new Seat("free", col, row));
+      }
+    }
     if (reservedSeats != undefined) {
-      this.reservedSeats = reservedSeats;
+      for (let reservedSeat of reservedSeats) {
+        let seatArray = seat.split('_');
+        let col = parseInt(seatArray[0]);
+        let row = parseInt(seatArray[1]);
+        for (let seat of this.seats) {
+          if (seat.column === col && seat.row === row) {
+            seat.setSeatStatus("reserved");
+          }
+          break;
+        }
+      }
     }
   }
 }
