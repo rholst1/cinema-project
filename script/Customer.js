335aa6a7ca6dd602bf8d9313a713f24abd8e2ab0
Jesper <lemuringen91113@gmail.com>
2021-02-10 12:52:46 +0100

    Added a Booking class that contains an array of seats and a Showings reference.

--- script/Customer.js
+++ script/Customer.js
@@ -1,7 +1,11 @@
 export default class Customer {
-  constructor(name, email, phoneNumber) {
+  constructor(name, email, phoneNumber, bookings) {
     this.name = name;
     this.email = email;
     this.phoneNumber = phoneNumber;
+    this.bookings = bookings;
+    if (this.bookings == undefined) {
+      this.bookings = [];
+    }
   }
 }
