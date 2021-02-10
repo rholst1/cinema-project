fafbe02d34c3e06f5df02e3f6c384b3e8e959a76
Jesper <lemuringen91113@gmail.com>
2021-02-10 13:02:36 +0100

    Made a comment for constructor usage in Customer. Added optional argument of array of Booking-objects.

--- script/Customer.js
+++ script/Customer.js
@@ -1,4 +1,8 @@
 export default class Customer {
+  /* Parameter expects: (String, String, String, []) - in order: 
+  customer name, customer email address, customer phone number,
+  a list of references to Booking-objects. The last argument is 
+  optional.*/
   constructor(name, email, phoneNumber, bookings) {
     this.name = name;
     this.email = email;
