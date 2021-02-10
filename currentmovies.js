73260f92417d4574aa58a511a3819fea0e19acd6
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-02-10 21:14:02 +0100

    Changed the html structor for currentmovie function
    Code along with Rasmus, we created new html and js files for the more info page, created class and function for getting the information to fill the page

--- currentmovies.js
+++ currentmovies.js
@@ -49,12 +49,12 @@ function addButtons() {}
 function buildMoviePage() {
   let movieHtml;
   while (i < movieTitles.length) {
-    movieHtml = `<div class="currentMovieIMG1">
+    movieHtml = /*html*/ `<div class="currentMovieIMG1">
                   <img src="${movieImages[i]}" class="img1" alt="Här ska en bild vara">
                 </div>
                 <div class="currentMovieTitleDiv">
                     <a href = "/moremovieinfo.html" class="movie-link"><h2>${movieTitles[i]}</h2></a><article class="currentMovieTitleContainer"> <p class="title-p"> ${movieGenre[i]} | ${movieLength[i]} | ${ageMin[i]} </p>
-                    </p><article><button class="generalButton" onclick = "buttonPage()">Mer info</button><button class="generalButton" onclick="#">Köp biljett</button></article></article><hr>
+                    <article><button class="generalButton" onclick = "buttonPage()">Mer info</button><button class="generalButton" onclick="#">Köp biljett</button></article></article><hr>
                       <p>${movieInfo[i]}
                       </p>
                   </div>`;
