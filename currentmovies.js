fddd2d8014daf13a169161ac890520ba7031d576
Rasmus <63317079+Razberryjam@users.noreply.github.com>
2021-02-16 17:04:43 +0100

    Code along with Matcus - changes to currentmovies and moreinfo
    Made some changes in how we build the page. Made it dynamically insert elements depending on how many movies we get from the database.

--- currentmovies.js
+++ currentmovies.js
@@ -1,70 +1,53 @@
-let main = `<main></main > `;
-$('header').after(main);
+$('header').after(`<main></main > `);
+$('main').append(`<section class="movies"></section>`);
+$('.movies').prepend(
+  `<h1 class="currentMovieTitleH1">AKTUELLA FILMER JUST NU</h1>`
+);
 
-let section = `<section class="movies">
-</section>
-`;
-$('main').append(section);
-
-let movieDivs = [
-  `<div class="currentMovie1"></div>`,
-  `<div class="currentMovie2"></div>`,
-  `<div class="currentMovie3"></div>`,
-  `<div class="currentMovie4"></div>`,
-  `<div class="currentMovie5"></div>`,
-  `<div class="currentMovie6"></div>`,
-];
-$('section').append(movieDivs);
-
-let movieTitles = ['COMMANDO', 'THE BATMAN', 'SÄLLSKAPSRESAN'];
-let movieImages = [
-  'img/commando2.jpg',
-  'img/batman.webp',
-  'img/sallskapsresan.jpg',
-];
-
-let movieGenre = ['Action', 'Action', 'Komedi'];
-let movieInfo = [
-  `En pensionerad specialagent vid namn John Matrix ledde en
-                  elitenhet och har lämnat de väpnade styrkorna för att bo i
-                  ett avskilt bergshem  med sin dotter Jenny.
-                  Men nu tvingas han gå ur pension när hans dotter
-                  kidnappas av ett band av skurkar som vill hämnas!
-                  Utan att Matrixtill dödas medlemmarna i hans tidigare
-                  enhet en efter en. Även om Matrix vän General Franklin Kirby ger Matrix beväpnade vakter, lyckas angripare kidnappa Matrix och Jenny.`,
-  `Batman info`,
-  `Välkommen till våran ö, sluta tänk på regn och snö. Sola, bada och ha  kul med Suntrip över jul! Om du vill ha nattklubbssväng, häng då med i Gabbes gäng.`,
-];
-let movieLength = ['1 tim 45 min', '1 tim 57 min', '1 tim 12 min'];
-let ageMin = ['15 år', '15 år', '6 år'];
-let pageLink = [``];
-let i = 0;
-
-let currentTitle = `<h1 class="currentMovieTitleH1">AKTUELLA FILMER JUST NU</h1>`;
-
-$('.movies').prepend(currentTitle);
-
-function addButtons() {}
-
-function buildMoviePage() {
-  let movieHtml;
-  while (i < movieTitles.length) {
-    movieHtml = /*html*/ `<div class="currentMovieIMG1">
-                  <img src="${movieImages[i]}" class="img1" alt="Här ska en bild vara">
-                </div>
-                <div class="currentMovieTitleDiv">
-                    <a href = "/moremovieinfo.html" class="movie-link"><h2>${movieTitles[i]}</h2></a><article class="currentMovieTitleContainer"> <p class="title-p"> ${movieGenre[i]} | ${movieLength[i]} | ${ageMin[i]} </p>
-                    <article><button class="generalButton" onclick = "buttonPage()">Mer info</button><button class="generalButton" onclick="#">Köp biljett</button></article></article><hr>
-                      <p>${movieInfo[i]}
-                      </p>
-                  </div>`;
-    i++;
-    $(`.currentMovie${i}`).append(movieHtml);
+let movieDivs = [];
+function createDivs(numberOfMovies) {
+  for (let i = 1; i < numberOfMovies + 1; i++) {
+    movieDivs.push(`<div class="currentMovie${i}"></div>`);
   }
+  $('section').append(movieDivs);
 }
 
+let movies;
+async function getMovies() {
+  movies = await db.run(`SELECT * FROM new_movie_list`);
+
+  buildMoviePage();
+}
+
+function buildMoviePage(selectedMovie) {
+  createDivs(movies.length);
+  let i = 0;
+  while (i < movieDivs.length) {
+    for (let {
+      title,
+      description,
+      length,
+      genres,
+      ageGroup,
+      images,
+    } of movies) {
+      let movieHtml;
+
+      movieHtml = /*html*/ `<div class="currentMovieIMG1">
+                  <img src="${images}" class="img1" alt="Här ska en bild vara">
+                </div>
+                <div class="currentMovieTitleDiv">
+                    <a href = "/moremovieinfo.html" class="movie-link"><h2>${title}</h2></a><article class="currentMovieTitleContainer"> <p class="title-p"> ${genres} | ${length} | ${ageGroup} </p>
+                    <article><button class="generalButton" onclick = "buttonPage()">Mer info</button><button class="generalButton" onclick="#">Köp biljett</button></article></article><hr>
+                      <p>${description}
+                      </p>
+                  </div>`;
+      i++;
+      $(`.currentMovie${i}`).append(movieHtml);
+    }
+  }
+}
 function buttonPage() {
   window.location.href = '/moremovieinfo.html';
 }
-
-buildMoviePage();
+getMovies();
