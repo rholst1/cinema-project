d2644c30683e322c0fb1063d5c2ca98dbc288888
Sena A.H <sena@Senas-Air.lan>
2021-02-17 16:54:03 +0100

    Adding method to listen to button

--- my-bookings-page.js
+++ my-bookings-page.js
@@ -33,3 +33,34 @@ $('.col2').append(
    <li>(Salong 1) film: Hidden kl 17.00 19/3-2021 </li>
    </ul>`);
 
+function listenToEmailButton() {
+  $("form").on({
+    click: function () {
+
+      console.log("hej knapp");
+      $('form').after(`<div class="maindiv"></div>`);
+
+      $('.maindiv').prepend(
+        `<br><br> <section class="col1"> <h1> Aktuella bokningar: </h1></section> <br> <hr> <br>
+  <section class="col2"> <h1>  Bokningshistorik:  </h1> </section>`);
+
+      $('.col1').append(
+        `<ul>
+   <li>(Salong 1) film: Hidden Figures kl 17.00 15/3-2021 </li>
+   <li>(Salong 2) film: Hidden Fig kl 18.00 17/3-2021 </li>
+   <li>(Salong 1) film: Hidden kl 17.00 19/3-2021 </li>
+   </ul>`);
+
+      $('.col2').append(
+        `<ul>
+   <li>(Salong 1) film: Hidden Figures kl 17.00 15/3-2021 </li>
+   <li>(Salong 2) film: Hidden Fig kl 18.00 17/3-2021 </li>
+   <li>(Salong 1) film: Hidden kl 17.00 19/3-2021 </li>
+   </ul>`);
+
+      //eventuell annan kod du vill köra
+    }
+  }, '.email-button');
+}
+
+listenToEmailButton();
