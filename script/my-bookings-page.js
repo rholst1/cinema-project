e426d629fab9ff14f51255dbab10c627948c2228
Sena A.H <sena@Senas-Air.lan>
2021-02-17 12:50:52 +0100

    changed script tag in html

---
+++ script/my-bookings-page.js
@@ -0,0 +1,8 @@
+
+$('header').after(`<main></main>`);
+
+$('main').append(`<section class="bookings"></section>`);
+
+$('.bookings').prepend(
+  `<h1 class="currentMovieTitleH1">MINA BOKNINGAR</h1>`
+);
