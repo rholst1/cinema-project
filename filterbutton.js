211fc51f942a59471c7e8da646729d11a6e84786
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-02-18 12:39:28 +0100

    function for hiding and showing movies with buttons
    Created a filter for the movie-page where u can choose the age and only the movies with that agegroup will show.

---
+++ filterbutton.js
@@ -0,0 +1,10 @@
+let $btns = $('.btn').click(function () {
+  if (this.id == 'all') {
+    $('.currentMovies > .movie-container').fadeIn(450);
+  } else {
+    let $age = $('.' + this.id).fadeIn(450);
+    $('.currentMovies > .movie-container').not($age).hide();
+  }
+  $btns.removeClass('active');
+  $(this).addClass('active');
+})
