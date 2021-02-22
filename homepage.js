0be477337d6053ec74fbf3d95c1fd3c0f950a105
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-02-22 12:41:10 +0100

    moved the carousel script for cleaner files

--- homepage.js
+++ homepage.js
@@ -1,3 +1,5 @@
+$.getScript('script/carousel.js');
+
 $('header').after(`<main></main > `);
 $('main').append(`<div class="slideshow-container"></div>`);
 $('.slideshow-container').append(buildCarousel());
@@ -93,36 +95,3 @@ function buildCarousel() {
 
   return carousel;
 }
-
-/* Functions that changes the slides and dots */
-let slideIndex = 1;
-
-function changeSlide(slideNumber) {
-  slideShow(slideIndex += slideNumber);
-}
-
-function changeDotSlide(dotNumber) {
-  slideShow(slideIndex = dotNumber);
-}
-
-function slideShow(slideNumber) {
-  let i;
-  let slide = document.getElementsByClassName("slides");
-  let dots = document.getElementsByClassName("dot");
-
-  if (slideNumber > slide.length) {
-    slideIndex = 1;
-  }
-  if (slideNumber < 1) {
-    slideIndex = slide.length;
-  }
-  for (i = 0; i < slide.length; i++) {
-    slide[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slide[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
-}
-slideShow(slideIndex);
---
+++ script/carousel.js
@@ -0,0 +1,32 @@
+/* Functions that changes the slides and dots */
+let slideIndex = 1;
+
+function changeSlide(slideNumber) {
+  slideShow(slideIndex += slideNumber);
+}
+
+function changeDotSlide(dotNumber) {
+  slideShow(slideIndex = dotNumber);
+}
+
+function slideShow(slideNumber) {
+  let i;
+  let slide = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+
+  if (slideNumber > slide.length) {
+    slideIndex = 1;
+  }
+  if (slideNumber < 1) {
+    slideIndex = slide.length;
+  }
+  for (i = 0; i < slide.length; i++) {
+    slide[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slide[slideIndex - 1].style.display = "block";
+  dots[slideIndex - 1].className += " active";
+}
+slideShow(slideIndex);
