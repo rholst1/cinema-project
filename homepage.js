095d2daa9f90e0e2abcedfad92b85f0b9c03c74d
Stefan Holmqvist <44743387+sholmqvist@users.noreply.github.com>
2021-02-23 14:47:11 +0100

    Restructure "Visas just nu"

--- homepage.js
+++ homepage.js
@@ -1,7 +1,9 @@
 $('header').after(`<main></main > `);
 $('main').append(`<section class="newsAndShowtimeElements"></section>`);
 $('.newsAndShowtimeElements').append(buildNews());
-$('.newsAndShowtimeElements').append(buildNowShowing());
+//$('.newsAndShowtimeElements').append(buildNowShowing());
+
+buildNowShowing();
 
 /* Function that builds the "Nyheter" section with the bullet point news */
 function buildNews() {
@@ -34,6 +36,42 @@ function buildNews() {
   return newsHtml;
 }
 
+/* Function that loads DB entries from Showings into Now Showing box */
+
+let showings = [];
+async function databasePullShowings() {
+  try {
+    showings = await db.run(`SELECT * FROM Showings`);
+  }
+  catch (err) {
+    console.log("Failed to load from Database");
+  }
+  console.table(showings);
+  console.log(showings[0].filmID);
+
+  let i = 0;
+  let showingsTmp = [];
+
+  for (let { filmID, auditorium, date, time } of showings) {
+
+    if (date.localeCompare("2021-03-2") === 0) {
+      buildHtmlElements(filmID, auditorium, time);
+
+      console.log(filmID, auditorium, date, time);
+    }
+
+
+  }
+
+
+}
+
+function buildHtmlElements(filmID, auditorium, time) {
+  let html = /*html*/ `<div class=showtoday><p color=white>${filmID} | ${time} | Salong ${auditorium}</p></div>
+  `;
+  $('.nowShowing').append(html);
+}
+
 /* Function that builds the "Visas just nu" section with links etc */
 function buildNowShowing() {
 
@@ -42,21 +80,24 @@ function buildNowShowing() {
   nowShowingHtml = /*html*/ `<div class="nowShowing">
       <h2 class="nowShowingh2">Visas just nu</h2>
       <ul class="nowShowingTitles">
-        <li>
-          <a href="ticketbooking.html" style="color: ghostwhite" class="hoverable">COMMANDO | 18:00 Sal 1</a>
-        </li>
-        <li>
-          <a href="ticketbooking.html" style="color: ghostwhite" class="hoverable">Sällskapsresan | 19:00 Sal 2</a>
-        </li>
-        <li>
-          <a href="ticketbooking.html" style="color: ghostwhite" class="hoverable">Hot Rod | 21:00 Sal 1</a>
-        </li>
-        <li>
-          <a href="ticketbooking.html" style="color: ghostwhite" class="hoverable">Interstellar | 21:30 Sal 2</a>
-        </li>
-      </ul>
-    </div>`
+    //     <li>
+    //       <a href="ticketbooking.html" style="color: ghostwhite" class="hoverabe"l>COMMANDO | 18:00 Sal 1</a>
+    //     </li>
+    //     <li>
+    //       <a href="ticketbooking.html" style="color: ghostwhite" class="hoverable">Sällskapsresan | 19:00 Sal 2</a>
+    //     </li>
+    //     <li>
+    //       <a href="ticketbooking.html" style="color: ghostwhite" class="hoverable">Hot Rod | 21:00 Sal 1</a>
+    //     </li>
+    //     <li>
+    //       <a href="ticketbooking.html" style="color: ghostwhite" class="hoverable">Interstellar | 21:30 Sal 2</a>
+    //     </li>
+    //   </ul>
+     </div>`;
 
-  return nowShowingHtml;
+  $('.newsAndShowtimeElements').append(nowShowingHtml, databasePullShowings());
+
+
+
 }
 
