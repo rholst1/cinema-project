7c6a833c40383d5113a88fe9cda7ac9e7dc33453
Rasmus <63317079+Razberryjam@users.noreply.github.com>
2021-02-24 10:04:33 +0100

    Rewrote function to get info from database and display it
    made the runQuery function

--- my-bookings-page.js
+++ my-bookings-page.js
@@ -1,54 +1,61 @@
-
 $('header').after(`<main></main>`);
 
 $('main').append(`<section class="bookings"></section>`);
 
-$('.bookings').prepend(
-  `<h1 class="currentMovieTitleH1">MINA BOKNINGAR</h1>`
-);
+$('.bookings').prepend(`<h1 class="currentMovieTitleH1">MINA BOKNINGAR</h1>`);
 
 $('main').append(`<div class="space"></div>`);
 
-$('.space').prepend(
-  `<br> <br>`
-);
+$('.space').prepend(`<br> <br>`);
 
 function listenToEmailButton() {
-  $("form").on({
-    click: function () {
+  $('form').on(
+    {
+      click: function () {
+        console.log('hej knapp');
+        $('form').after(`<div class="maindiv"></div>`);
 
-      console.log("hej knapp");
-      $('form').after(`<div class="maindiv"></div>`);
+        $('.maindiv').prepend(
+          `<br><br> <section class="col2"> <h1>  Bokningshistorik:  </h1> </section>`
+        );
 
-      $('.maindiv').prepend(
-        `<br><br> <section class="col2"> <h1>  Bokningshistorik:  </h1> </section>`);
+        queryDatabase();
 
-      $('.col2').append(
-        `<ul>
+        $('.col2').append(
+          `<ul>
   
-   </ul>`);
-
-    }
-  }, '.email-button');
+   </ul>`
+        );
+      },
+    },
+    '.email-button'
+  );
 }
-
 
 listenToEmailButton();
 
 let runQuery;
 
+<<<<<<< Updated upstream
 async function getBookings() {
 
 
   let inputEmail = document.getElementById("emailInput").value;
 
   runQuery = await db.run(/*sql*/`
+=======
+//Runs when user clicks "Hämta bokningar"
+async function queryDatabase() {
+  let inputEmail = document.getElementById('emailInput').value;
+  runQuery = await db.run(/*sql*/ `
+>>>>>>> Stashed changes
     SELECT * FROM Showings JOIN bookingHistory ON showingsID WHERE ID = showingsID AND email = '${inputEmail}';
   `);
 
   console.log(runQuery);
   console.table(runQuery);
 
+<<<<<<< Updated upstream
   for (let { ID, filmID, auditorium, date, time, email, seats, showingsID } of runQuery) {
     // let [ID, filmID, auditorium, date, time, email, seats, showingsID] = runQuery[i];
 
@@ -62,17 +69,43 @@ async function getBookings() {
 
 
 
+=======
+  for (let {
+    ID,
+    filmID,
+    auditorium,
+    date,
+    time,
+    email,
+    seats,
+    showingsID,
+  } of runQuery) {
+    let queryHtml = /*HTML*/ `<div><p color=white>${date} ${time} ${filmID} ${auditorium} ${seats}</p></div>`;
+    $('ul').append(queryHtml);
+>>>>>>> Stashed changes
   }
-  let unpacked = runQuery.map(
-    ([ID, filmID, auditorium, date, time, email, seats, showingsID]) => ({ ID, filmID, auditorium, date, time, email, seats, showingsID })
-  );
-  console.log('unpacked', unpacked);
-
-
-  // for (let { ID, filmID, auditorium, date, time, email, seats, showingsID } of unpacked) {
-  //   $('.col2 ul').append(
-  //   `<li> Salong: ${auditorium} film: ${filmID} sittplats: ${seat} Datum och tid: ${date} ${time} </li>`);
-  // }
-
 }
-getBookings();
+
+// async function getBookings() {
+//   let inputEmail = document.getElementById('emailInput').value;
+
+//   let unpacked = runQuery.map(
+//     ([ID, filmID, auditorium, date, time, email, seats, showingsID]) => ({
+//       ID,
+//       filmID,
+//       auditorium,
+//       date,
+//       time,
+//       email,
+//       seats,
+//       showingsID,
+//     })
+//   );
+//   console.log('unpacked', unpacked);
+
+// for (let { ID, filmID, auditorium, date, time, email, seats, showingsID } of unpacked) {
+//   $('.col2 ul').append(
+//   `<li> Salong: ${auditorium} film: ${filmID} sittplats: ${seat} Datum och tid: ${date} ${time} </li>`);
+// }
+//}
+// getBookings();
