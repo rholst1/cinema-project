27bca76e90cd9f98d59d2725ea78e25c6d1766c5
Marcus Zarabi <zarabi.marcus@hotmail.com>
2021-02-25 14:37:19 +0100

    function to build the auditorium depending on wich salong is choosen

--- newBookingScript/newBookingMain.js
+++ newBookingScript/newBookingMain.js
@@ -28,9 +28,11 @@ async function addMovies() {
 
 async function renderMovieBooking(i) {
   $('.selectedShowing').remove();
+  $('h1').remove();
+  $('.layout').remove();
   let queryShowings = await db.run(/*sql*/ `SELECT * FROM Showings WHERE filmID = '${movies[i].title}'`)
 
-  $('h1').replaceWith(`<h1>${movies[i].title}</h1>`)
+  $('.showings-container').append(`<h1>${movies[i].title}</h1>`)
   for (let { auditorium, date, time } of queryShowings) {
     $('.showings-container').append(/*html*/`<div class="selectedShowing"><div><h3>Salong: ${auditorium} | Datum: ${date} | Time: ${time}</h3></div><div class="book"><button id="book" class="general-button">Boka</button></div></div>`);
   }
@@ -48,6 +50,7 @@ function hideMenu() {
 };
 
 $(document).on("click", "#book", function () {
+  $('h1').remove();
   $('.selectedShowing').remove();
 
   renderSeatChooser();
@@ -55,8 +58,34 @@ $(document).on("click", "#book", function () {
 });
 
 
-function renderSeatChooser() {
-  $('.showings-container').after()
+async function renderSeatChooser() {
+  $('.layout').remove();
+  $('.showings-container').after(`<div class="layout"></div>`)
+  $('.layout').append(`<div class="container">
+  	<div class="screen"></div>`)
 
 
+  let auditorium = await db.run(/*sql*/`SELECT * FROM Auditorium`);
+
+  for (k = 0; k < auditorium[0].rows; k++) {
+    $('.container').append(`<div class="row"></div>`)
+  }
+  for (l = 0; l < auditorium[0].seatsPerRow; l++) {
+    $('.row').append(`<div class="seat"></div>`)
+  }
+  $('.container').append(`<div class="showcase">
+		<div>
+			<div class="seatshow"></div>
+			<span>N/A</span>
+		</div>
+		<div>
+			<div class="seatshow selected"></div>
+			<span>Valda platser</span>
+		</div>
+		<div>
+			<div class="seatshow occupied"></div>
+			<span>Upptagna</span>
+		</div>
+	</div>`)
 }
+
