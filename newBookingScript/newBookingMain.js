6b101e62fa503beb5ee4e47aba450ebbb3581c4a
Rasmus <63317079+Razberryjam@users.noreply.github.com>
2021-03-01 16:48:27 +0100

    Code along with Marcus: Added querys to book/get confirmation.
    DB breaksdown every now and then, works about 80% of the time upon page reload.

--- newBookingScript/newBookingMain.js
+++ newBookingScript/newBookingMain.js
@@ -1,13 +1,13 @@
 $('header').after(/*html*/ `<main></main>`);
-$('main').append(
-  /*html*/ `<div class="choose-movie"></div>`
-);
-$('.choose-movie').append(/*html*/`<div class="dropdown">
+$('main').append(/*html*/ `<div class="choose-movie"></div>`);
+$('.choose-movie').append(/*html*/ `<div class="dropdown">
   <button id="toggle" class="general-button dropbtn">Välj film</button>
   <div class="dropdown-content">
   </div>
 </div>`);
-$('.choose-movie').after(/*html*/`<div class="showings-container"><h1></h1></div>`);
+$('.choose-movie').after(
+  /*html*/ `<div class="showings-container"><h1></h1></div>`
+);
 
 let movies;
 let id;
@@ -17,25 +17,24 @@ let adultTickets = 0;
 let seniorTickets = 0;
 let totalAmountTickets = 0; // Userinput
 let selectedSeatNrArray = [];
-let selectedShow = 1;
 let showingID;
 
 function renderChosenTicketsC() {
   $('.showAmountC').remove();
   let html = /*html*/ `<p class="showAmountC">${childTickets}</p>`;
- $('.amountC').append(html);
+  $('.amountC').append(html);
 }
 
 function renderChosenTicketsA() {
   $('.showAmountA').remove();
   let html = /*html*/ `<p class="showAmountA">${adultTickets}</p>`;
- $('.amountA').append(html);
+  $('.amountA').append(html);
 }
 
 function renderChosenTicketsS() {
   $('.showAmountS').remove();
   let html = /*html*/ `<p class="showAmountS">${seniorTickets}</p>`;
- $('.amountS').append(html);
+  $('.amountS').append(html);
 }
 
 function addChildTicket() {
@@ -145,7 +144,7 @@ async function renderSeatChooser(showingID) {
     }
   }
 
-  $('.container').append(/*html*/`<div class="showcase">
+  $('.container').append(/*html*/ `<div class="showcase">
 		<div>
 			<div class="seatshow"></div>
 			<span>N/A</span>
@@ -187,31 +186,32 @@ function renderTicketChooser() {
   let htmlChild = /*html*/ `<span>Barn - 75kr</span><div class="button-container"><button id="childTicketRem" class="general-button">-</button><div class="amountC"><p class="showAmountC">0</p></div><button id="childTicketAdd" class="general-button">+</button></div>`;
 
   let htmlAdult = /*html*/ `<span>Vuxen - 85kr</span><div class="button-container"><button id="adultTicketRem" class="general-button">-</button><div class="amountA"><p class="showAmountA">0</p></div><button id="adultTicketAdd" class="general-button">+</button></div>`;
-  
+
   let htmlSenior = /*html*/ `<span>Pensionär - 65kr</span><div class="button-container"><button id="seniorTicketRem" class="general-button">-</button><div class="amountS"><p class="showAmountS">0</p></div><button id="seniorTicketAdd" class="general-button">+</button></div>`;
-    
-  let htmlButton = /*html*/ `<div><button id="continue-button" class="general-button">Fortsätt</button></div>`
+
+  let htmlButton = /*html*/ `<div><button id="continue-button" class="general-button">Fortsätt</button></div>`;
 
   let html = htmlChild + htmlAdult + htmlSenior;
   $('.ticket-container').append(html);
   $('.continue-container').append(htmlButton);
 }
 
-
 $(document).on('click', '.seat', function () {
   if ($(this).hasClass('seat') && !$(this).hasClass('occupied')) {
     if ($(this).hasClass('seat') && $(this).hasClass('selected')) {
       $(this).toggleClass('selected');
-      function removeFromListByIndex(index) { selectedSeatNrArray.splice(index, 1); }
+      function removeFromListByIndex(index) {
+        selectedSeatNrArray.splice(index, 1);
+      }
       index = selectedSeatNrArray.indexOf($(this).get(0).id);
       removeFromListByIndex(index);
-      actualBooking(selectedSeatNrArray)
+      actualBooking(selectedSeatNrArray);
     } else if (!(selectedSeatNrArray.length === totalAmountTickets)) {
       if ($(this).hasClass('seat') && !$(this).hasClass('occupied')) {
         $(this).toggleClass('selected');
         selectedSeatNrArray.push($(this).get(0).id);
       }
-      actualBooking(selectedSeatNrArray)
+      actualBooking(selectedSeatNrArray);
     } else {
       alert('Du har valt för många din jävel');
     }
@@ -220,52 +220,74 @@ $(document).on('click', '.seat', function () {
 
 function actualBooking(selectedSeatNrArray) {
   $('.actual-booking').remove();
-  $('.layout').after(/*html*/`<div class="actual-booking"><p class ="platser">Valda platser: </p></div>`)
+  $('.layout').after(
+    /*html*/ `<div class="actual-booking"><p class ="platser">Valda platser: </p></div>`
+  );
   for (i = 0; i < selectedSeatNrArray.length; i++) {
-    let seat = selectedSeatNrArray[i].match(/\d/g).join("");
+    let seat = selectedSeatNrArray[i].match(/\d/g).join('');
     $('.platser').append(`<span class="seatSpan${i}">${seat}</span>`);
-       if (i > 0) {
-        $(`.seatSpan${i}`).prepend(`, `);
+    if (i > 0) {
+      $(`.seatSpan${i}`).prepend(`, `);
     }
   }
 }
 
 function inputInfo() {
-  $('.layout').after(/*html*/`<div class="booking-form"><form id="form" class="book-tickets">
+  $('.layout')
+    .after(/*html*/ `<div class="booking-form"><form id="form" class="book-tickets">
     <input type="email" placeholder="Ange email..." id="email" />
     <input type="tel" placeholder="Ange telefonnummer..." id="phonenumber" />
     <button type="submit" class="general-button" id="bookbtn" >Boka</button>
   </form></div>`);
- $('#form').submit(function() {
-    if ($.trim($("#email").val()) === "") {
-        alert('Var vänlig och fyll i alla fälten, tack!');
-        return false;
+  $('#form').submit(function () {
+    if ($.trim($('#email').val()) === '') {
+      alert('Var vänlig och fyll i alla fälten, tack!');
+      return false;
     } else {
-      let email = $("#email").val();
-      let phonenumber = $("#phonenumber").val();
-      let price = (childTickets * 75)+(adultTickets * 85)+(seniorTickets * 65);
-      saveSeats(selectedSeatNrArray);
+      let email = $('#email').val();
+      let phonenumber = $('#phonenumber').val();
+      let price = childTickets * 75 + adultTickets * 85 + seniorTickets * 65;
       saveBooking(email, phonenumber, price);
+      saveSeats(selectedSeatNrArray, email);
       // Ändra bekräftelse till valda stolsnummer, film och datum.
-      alert('Tack för din bokning!')
-      
     }
- });
+  });
 }
 
 async function saveBooking(email, phonenumber, price) {
-  db.run("BEGIN TRANSACTION");
-  await db.run(/*sql*/`INSERT INTO Bookings (phonenumber, email, showingID, price) VALUES ('${phonenumber}', '${email}', ${showingID}, ${price})`);
+  db.run('BEGIN TRANSACTION');
+  await db.run(
+    /*sql*/ `INSERT INTO Bookings (phonenumber, email, showingID, price) VALUES ('${phonenumber}', '${email}', ${showingID}, ${price})`
+  );
+  db.run('COMMIT');
 }
-async function saveSeats(seatings) {
-  for (i = 0; i < seatings.length; i++){
-    db.run("BEGIN TRANSACTION")
-    let seat = seatings[i].match(/\d/g).join("");
-    await db.run(/*sql*/`UPDATE Seatings SET status = "occupied" WHERE seatNumber = ${seat} AND showingID = ${showingID}`);
+async function saveSeats(seatings, email) {
+  console.log('hej från saveSeats', showingID);
+  let bookingID = await db.run(
+    /*sql*/ `SELECT ID FROM Bookings WHERE email = '${email}' AND showingID = ${showingID}`
+  );
+  console.log(bookingID);
+  let bookingIdNewest = 0;
+  for (let { ID } of bookingID) {
+    if (bookingIdNewest < ID) {
+      bookingIdNewest = ID;
+    }
   }
-  db.run("COMMIT");
+  console.log(bookingIdNewest);
+
+  for (i = 0; i < seatings.length; i++) {
+    db.run('BEGIN TRANSACTION');
+    let seat = seatings[i].match(/\d/g).join('');
+    await db.run(
+      /*sql*/ `UPDATE Seatings SET status = "occupied", bookingID = ${bookingIdNewest} WHERE seatNumber = ${seat} AND showingID = ${showingID}`
+    );
+  }
+
+  db.run('COMMIT');
+  console.log('skicka till query', bookingIdNewest);
+  renderConfirmation(bookingIdNewest);
 }
-db.run("COMMIT");
+//db.run('COMMIT');
 $(document).on('click', '#childTicketAdd', function () {
   addChildTicket();
 });
@@ -290,9 +312,45 @@ $(document).on('click', '#continue-button', function () {
   renderSeatChooser(showingID);
   inputInfo();
   actualBooking();
-  
-
 });
-  //$('#childTicketAdd').click(addChildTicket());
-  // $('#childTicketRem').click(remChildTicket());
 
+async function renderConfirmation(bookingIdNewest) {
+  console.log('innan query', bookingIdNewest);
+  db.run('BEGIN TRANSACTION');
+  let confirmation = await db.run(
+    /*sql*/ `SELECT DISTINCT Showings.filmID, Showings.date, Showings.time, Seatings.seatNumber, Showings.auditorium FROM Showings INNER JOIN Bookings ON (Bookings.showingID = Showings.ID) INNER JOIN Seatings ON (Seatings.bookingID = ${bookingIdNewest}) WHERE Showings.ID = ${showingID}`
+  );
+  db.run('COMMIT');
+  let { filmID, date, time, auditorium } = confirmation[0];
+  $('.layout').replaceWith(
+    `<div class="tack" ><h1>Tack för din bokning</h1></div>`
+  );
+  $('.actual-booking').remove();
+  $('.booking-form').remove();
+  $('.tack').append(`
+    <div class="färdigbokningLOL">
+      <h2>${filmID}</h2>
+      <h3>
+        ${date} ${time} 
+      </h3>
+      <h3>Salong: ${auditorium}</h3>
+      <h3 class="stol"> Stolsnummer: </h3>
+    </div>`);
+  console.log(confirmation);
+
+  let n = 0;
+  for (let { seatNumber } of confirmation) {
+    console.log(seatNumber);
+    let html = '';
+
+    if (n > 0) {
+      console.log('made it here');
+      html += ' ,';
+    }
+    html += `${seatNumber}`;
+    $('.stol').append(`${html}`);
+    n++;
+  }
+}
+
+// SELECT Showings.filmID, Showings.date, Showings.time, Seatings.seatNumber, Showings.auditorium FROM Showings INNER JOIN Bookings ON (Bookings.showingID = Showings.ID) INNER JOIN Seatings ON (Seatings.bookingID = 1) WHERE Showings.ID = Bookings.showingID
