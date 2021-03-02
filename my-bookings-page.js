446737a6bee6f8b09c626b2bef2cb7d7f21d7ec4
Rasmus <63317079+Razberryjam@users.noreply.github.com>
2021-03-02 15:44:46 +0100

    Updated querys to work with new database changes
    Reworked querys and tweaked some functions.
    Added loopSeats()

--- my-bookings-page.js
+++ my-bookings-page.js
@@ -10,9 +10,8 @@ $('.space').prepend(`<br> <br>`);
 
 $('.space').after(`<form class="get-booking">
   <input type="text" class="other-button" placeholder="Ange email..." id="emailInput" />
-  <button type="button" class="generalButton hoverable email-button get-bookings">Hämta dina bokningar</button>
+  <button type="button" class="general-button hoverable email-button get-bookings">Hämta dina bokningar</button>
 </form>`);
-
 
 function listenToEmailButton() {
   $('form').on(
@@ -43,6 +42,7 @@ listenToEmailButton();
 let runQuery;
 let fullDate;
 let time;
+let bookingInfo;
 
 //currentDateAndTime();
 
@@ -50,84 +50,110 @@ let time;
 async function queryDatabase() {
   let inputEmail = document.getElementById('emailInput').value;
 
-  runQuery = await db.run(/*sql*/ `
-    SELECT * FROM Showings JOIN bookingHistory ON showingsID WHERE ID = showingsID AND email = '${inputEmail}';
-  `);
+  // runQuery = await db.run(/*sql*/ `
+  //   SELECT * FROM Showings JOIN bookingHistory ON showingsID WHERE ID = showingsID AND email = '${inputEmail}';
+  // `);
 
-  for (let {
-    ID,
-    filmID,
-    auditorium,
-    date,
-    time,
-    email,
-    seats,
-    showingsID,
-  } of runQuery) {
+  bookingInfo = await db.run(
+    /*sql*/ `SELECT DISTINCT Showings.filmID, Showings.date, Showings.time, Showings.auditorium, Bookings.price, Bookings.ID FROM Bookings INNER JOIN Showings ON(Bookings.showingID = Showings.ID) WHERE Bookings.email = '${inputEmail}'`
+  );
 
+  console.log('booking ID: ', bookingInfo);
+
+  for (let { filmID, auditorium, date, time, ID, price } of bookingInfo) {
     let today = new Date();
-    let leadingZero = today.getFullYear() + ('0' + (today.getMonth() + 1)).slice(-2) + ('0' + today.getDate()).slice(-2);
+    let leadingZero =
+      today.getFullYear() +
+      ('0' + (today.getMonth() + 1)).slice(-2) +
+      ('0' + today.getDate()).slice(-2);
     // console.log("date today with leading zero", leadingZero);
 
+    console.log(ID);
+    let seatResult = await db.run(
+      /*SQL*/ `SELECT * FROM Seatings WHERE Seatings.bookingID = ${ID}`
+    );
     let todayDateInt = parseInt(leadingZero);
-    console.log(" TODAY parsed int", todayDateInt);
+    console.log(' TODAY parsed int', todayDateInt);
 
     let parts = `${date}`.split('-');
     let movieDate = new Date(parts[0], parts[1] - 1, parts[2]);
 
-    let movieLeadingZero = movieDate.getFullYear() + ('0' + (movieDate.getMonth() + 1)).slice(-2) + ('0' + movieDate.getDate()).slice(-2);
-    console.log("MOVIE DATE WITH LEADING ZERO", movieLeadingZero);
+    let movieLeadingZero =
+      movieDate.getFullYear() +
+      ('0' + (movieDate.getMonth() + 1)).slice(-2) +
+      ('0' + movieDate.getDate()).slice(-2);
+    console.log('MOVIE DATE WITH LEADING ZERO', movieLeadingZero);
 
     let movieIntDate = parseInt(movieLeadingZero);
 
-    console.log("MOVIEDATE PARSED INT", movieIntDate);
+    console.log('MOVIEDATE PARSED INT', movieIntDate);
 
     let hourNow = today.getHours();
     let minutesNow = today.getMinutes();
 
-    let timeNowString = "" + ('0' + hourNow).slice(-2) + ('0' + minutesNow).slice(-2);
-    console.log("string time now", timeNowString);
+    let timeNowString =
+      '' + ('0' + hourNow).slice(-2) + ('0' + minutesNow).slice(-2);
+    console.log('string time now', timeNowString);
 
     let timeNowInt = parseInt(timeNowString);
-    console.log("time now int parsed!", timeNowInt);
+    console.log('time now int parsed!', timeNowInt);
 
     let movieTimeParts = `${time}`.split(':');
 
-    let movieTimeString = "" + ('0' + movieTimeParts[0]).slice(-2) + ('0' + movieTimeParts[1]).slice(-2);
-    console.log("movie time string", movieTimeString);
+    let movieTimeString =
+      '' +
+      ('0' + movieTimeParts[0]).slice(-2) +
+      ('0' + movieTimeParts[1]).slice(-2);
+    console.log('movie time string', movieTimeString);
 
     let movieTimeInt = parseInt(movieTimeString, 10);
-    console.log("movie time int parsed", movieTimeInt);
-
-
-    if ((movieIntDate < todayDateInt) || ((movieIntDate = todayDateInt) && (movieTimeInt < timeNowInt))) {
-      let queryHtml = /*HTML*/ `<li> Salong: ${auditorium} film: ${filmID} sittplats: ${seats} Datum och tid: ${date} ${time} </li> `;
+    console.log('movie time int parsed', movieTimeInt);
+    let increment = 1;
+    if (
+      movieIntDate < todayDateInt ||
+      ((movieIntDate = todayDateInt) && movieTimeInt < timeNowInt)
+    ) {
+      let queryHtml = /*HTML*/ `<li class="history"> Salong: ${auditorium} film: ${filmID} Datum och tid: ${date} ${time} Sittplatser:<span class="seats${increment}"></span> </li> `;
 
       $('ul').append(queryHtml);
-    }
-    else if ((movieIntDate = todayDateInt) && (movieTimeInt > timeNowInt) || ((movieIntDate > todayDateInt) && (movieTimeInt > timeNowInt))) {
-      let queryHtmlKommande = /*HTML*/ `<li class="kommandevisning"> Kommande visning: Salong: ${auditorium} Film: ${filmID} Sittplats: ${seats} Datum och tid: ${date} ${time} 
+      loopSeats(seatResult, increment);
+    } else if (
+      ((movieIntDate = todayDateInt) && movieTimeInt > timeNowInt) ||
+      (movieIntDate > todayDateInt && movieTimeInt > timeNowInt)
+    ) {
+      let queryHtmlKommande = /*HTML*/ `<li class="kommandevisning"> Kommande visning: Salong: ${auditorium} Film: ${filmID} Datum och tid: ${date} ${time} Sittplatser:<span class="seats${increment}"></span>
     <br> <button class="general-button removeButton" onclick="alert('Biljett avbokad!')" id="delete">Avboka</button></li>
     `;
 
-      $('ul').append(queryHtmlKommande);
+      $('ul').prepend(queryHtmlKommande);
     }
 
     $('.removeButton').on('click', async () => {
-      db.run("BEGIN TRANSACTION")
-      console.log("data", showingsID, email);
-      let result = await db.run(/*sql*/`
-      DELETE FROM bookingHistory WHERE showingsID = $showingsID AND email = $email;
-    `, { showingsID, email });
+      db.run('BEGIN TRANSACTION');
+      console.log('data', showingsID, email);
+      let result = await db.run(
+        /*sql*/ `
+      DELETE FROM Bookin WHERE showingsID = $showingsID AND email = $email;
+    `,
+        { showingsID, email }
+      );
 
-      console.log("result", result);
+      console.log('result', result);
     });
   }
 }
+function loopSeats(seatResult, increment) {
+  let n = 0;
+  for (let { seatNumber } of seatResult) {
+    let html = ' ';
 
-db.run("COMMIT");
+    if (n > 0) {
+      html += ' ,';
+    }
+    html += `${seatNumber}`;
+    $(`.seats${increment}`).append(`${html}`);
+    n++;
+  }
+}
 
-
-
-
-
+db.run('COMMIT');
