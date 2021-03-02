1c0324e91b6e228e481b12b68d541cd8d60cda82
Rasmus <63317079+Razberryjam@users.noreply.github.com>
2021-03-02 16:49:56 +0100

    Code along with Marcus: website now properly removes a booking
    When user enter email, website lists all bookings related to userinputmail, removebutton now properly removes and updates tables in the database.

--- my-bookings-page.js
+++ my-bookings-page.js
@@ -1,14 +1,16 @@
 $('header').after(`<main></main>`);
 
-$('main').append(`<section class="bookings"></section>`);
+$('main').append(/*html*/ `<section class="bookings"></section>`);
 
-$('.bookings').prepend(`<h1 class="currentMovieTitleH1">MINA BOKNINGAR</h1>`);
+$('.bookings').prepend(
+  /*html*/ `<h1 class="currentMovieTitleH1">MINA BOKNINGAR</h1>`
+);
 
-$('main').append(`<div class="space"></div>`);
+$('main').append(/*html*/ `<div class="space"></div>`);
 
-$('.space').prepend(`<br> <br>`);
+$('.space').prepend(/*html*/ `<br> <br>`);
 
-$('.space').after(`<form class="get-booking">
+$('.space').after(/*html*/ `<form class="get-booking">
   <input type="text" class="other-button" placeholder="Ange email..." id="emailInput" />
   <button type="button" class="general-button hoverable email-button get-bookings">Hämta dina bokningar</button>
 </form>`);
@@ -18,19 +20,21 @@ function listenToEmailButton() {
     {
       click: function () {
         $('.maindiv').remove();
-        $('form').after(`<div class="maindiv"></div>`);
+        $('form').after(/*html*/ `<div class="maindiv"></div>`);
 
-        $('.maindiv').prepend(
-          `<br><br> <section class="col2"> <h1>  Bokningar:  </h1> </section>`
+        $('.maindiv').append(
+          /*html*/
+          `<section class="col2"> <h1>Kommande visningar:</h1><hr></section>`
         );
 
         queryDatabase();
 
-        $('.col2').append(
-          `<ul>
-  
-   </ul>`
+        $('.col2').append(/*html*/ `<ul class="upcoming"></ul>`);
+        $('.maindiv').append(
+          /*html*/
+          `<section class="col3"> <h1>Historik:</h1><hr></section>`
         );
+        $('.col3').append(/*html*/ `<ul class="history"></ul>`);
       },
     },
     '.email-button'
@@ -51,17 +55,21 @@ let increment = 1;
 async function queryDatabase() {
   let inputEmail = document.getElementById('emailInput').value;
 
-  // runQuery = await db.run(/*sql*/ `
-  //   SELECT * FROM Showings JOIN bookingHistory ON showingsID WHERE ID = showingsID AND email = '${inputEmail}';
-  // `);
-
   bookingInfo = await db.run(
-    /*sql*/ `SELECT DISTINCT Showings.filmID, Showings.date, Showings.time, Showings.auditorium, Bookings.price, Bookings.ID FROM Bookings INNER JOIN Showings ON(Bookings.showingID = Showings.ID) WHERE Bookings.email = '${inputEmail}'`
+    /*sql*/ `SELECT DISTINCT Showings.filmID, Showings.date, Showings.time, Showings.auditorium, Bookings.price, Bookings.ID, Bookings.showingID FROM Bookings INNER JOIN Showings ON(Bookings.showingID = Showings.ID) WHERE Bookings.email = '${inputEmail}'`
   );
 
   console.log('booking ID: ', bookingInfo);
 
-  for (let { filmID, auditorium, date, time, ID, price } of bookingInfo) {
+  for (let {
+    filmID,
+    auditorium,
+    date,
+    time,
+    ID,
+    price,
+    showingID,
+  } of bookingInfo) {
     let today = new Date();
     let leadingZero =
       today.getFullYear() +
@@ -69,7 +77,7 @@ async function queryDatabase() {
       ('0' + today.getDate()).slice(-2);
     // console.log("date today with leading zero", leadingZero);
 
-    console.log(ID);
+    console.log('Showing ID:', showingID);
     let seatResult = await db.run(
       /*SQL*/ `SELECT * FROM Seatings WHERE Seatings.bookingID = ${ID}`
     );
@@ -114,37 +122,35 @@ async function queryDatabase() {
       movieIntDate < todayDateInt ||
       ((movieIntDate = todayDateInt) && movieTimeInt < timeNowInt)
     ) {
-      let queryHtml = /*HTML*/ `<li class="history"> Salong: ${auditorium} film: ${filmID} Datum och tid: ${date} ${time} Sittplatser:<span class="seats${increment}"></span> </li> `;
+      let queryHtml = /*html*/ `<li class="historik"> Salong: ${auditorium} film: ${filmID} Datum och tid: ${date} ${time} Sittplatser:<span class="seats${increment}"></span> </li> `;
 
-      $('ul').append(queryHtml);
+      $('.history').append(queryHtml);
       loopSeats(seatResult, increment);
     } else if (
       ((movieIntDate = todayDateInt) && movieTimeInt > timeNowInt) ||
       (movieIntDate > todayDateInt && movieTimeInt > timeNowInt)
     ) {
-      let queryHtmlKommande = /*HTML*/ `<li class="kommandevisning"> Kommande visning: Salong: ${auditorium} Film: ${filmID} Datum och tid: ${date} ${time} Sittplatser:<span class="seats${increment}"></span>
-    <br> <button class="general-button removeButton" onclick="alert('Biljett avbokad!')" id="delete">Avboka</button></li>
+      let queryHtmlKommande = /*html*/ `<li class="kommandevisning"> Salong: ${auditorium} Film: ${filmID} Datum och tid: ${date} ${time} Sittplatser:<span class="seats${increment}"></span>
+    <br> <button class="general-button removeButton" value="${ID}">Avboka</button></li>
     `;
 
-      $('ul').prepend(queryHtmlKommande);
+      $('.upcoming').append(queryHtmlKommande);
       loopSeats(seatResult, increment);
     }
-
-    $('.removeButton').on('click', async () => {
-      db.run('BEGIN TRANSACTION');
-      console.log('data', showingsID, email);
-      let result = await db.run(
-        /*sql*/ `
-      DELETE FROM Bookin WHERE showingsID = $showingsID AND email = $email;
-    `,
-        { showingsID, email }
-      );
-
-      console.log('result', result);
-    });
     increment++;
   }
 }
+
+$(document).on('click', '.removeButton', async function () {
+  db.run('BEGIN TRANSACTION');
+  let removeID = $(this).val();
+  console.log('made into remove button');
+  let result = await db.run(/*sql*/ `
+      DELETE FROM Bookings WHERE ID = ${removeID}; UPDATE Seatings SET status = 'empty' WHERE bookingID = ${removeID}`);
+
+  console.log('result', result);
+});
+
 function loopSeats(seatResult, increment) {
   let n = 0;
   for (let { seatNumber } of seatResult) {
