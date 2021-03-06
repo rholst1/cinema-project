43d27e6dee457ffc930806215065caa2c629a180
Stefan Holmqvist <44743387+sholmqvist@users.noreply.github.com>
2021-03-06 15:11:00 +0100

    Minor CSS Adjust on the link in now showing CSS

--- HomePage/script/homepage.js
+++ HomePage/script/homepage.js
@@ -74,7 +74,7 @@ async function databasePullShowings() {
 /* Function that builds the elements that gets loaded underneath "Visas Just Nu" Header */
 /* Lends the buildInfo method from Carousel to direct the user to the info page on the specific movie shown on the current day */
 function buildNowShowingElements(filmID, auditorium, time) {
-  let html = /*html*/ `<li><a href="#" onclick="buildInfo('${filmID}')">${filmID} | ${time} | Salong ${auditorium}</a></li>
+  let html = /*html*/ `<li><a href="#" onclick="buildInfo('${filmID}')">${filmID}</a> | ${time} | Salong ${auditorium}</li>
   `;
   $('.nowShowingTitles').append(html);
 }
